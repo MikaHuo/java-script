759408aa94558e77ddd3b95ec4d02cb4bf839997
Mika Huotari <mika.huotari@edu.keuda.fi>
2022-09-29 10:40:24 +0300

    v2t5

--- javav2/v2t5.js
+++ javav2/v2t5.js
@@ -1,4 +1,12 @@
-function laske()
+function lause()
 {
-
+  var luku = document.getElementById('lu').value;
+  var temp = 0;
+  var vastaus = "";
+  for(var i = 1; i <= 10; i++)
+  {
+    temp = luku * i;
+    vastaus += "luku " + luku + " * " + i + " = " + temp +"<br />";
+  }
+  document.getElementById('kertotaulu').innerHTML = vastaus;
 }
