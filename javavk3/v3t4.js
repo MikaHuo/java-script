9d2efba5277a75918bc9482776a6f63b5f307027
MikaHuo <mika.huotari@edu.keuda.fi>
2022-10-07 11:22:13 +0300

    v3t4

--- javavk3/v3t4.js
+++ javavk3/v3t4.js
@@ -1,5 +1,10 @@
 function v3t4()
 {
   var kertoma = document.getElementById('kertoma').value;
-
+  var laske = 1;
+  for(var i = 1; i<= kertoma; i++)
+{
+  laske*=i;
+}
+  document.getElementById('vastaus').innerHTML = " Luvun " + kertoma + " " + "kertoma on" + " " + laske;
 }
